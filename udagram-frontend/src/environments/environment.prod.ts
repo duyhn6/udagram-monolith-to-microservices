b84992677c32d8b27e935caed9e53aaad0a6066d
duyhn6 <duyhn.dn.01@gmail.com>
2023-06-10 12:52:41 +0700

    add screenshot and config

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://3.95.216.12:8080/api/v0'
+  apiHost: 'http://aced7941e13b1455bba526494e3f216a-419830311.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
